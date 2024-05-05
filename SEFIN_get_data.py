5279acf69e98615cad21cf72766609e845bc4dd4
Wederson Rodrigues de Souza <wedersonrsouza@gmail.com>
2024-05-05 15:43:19 -0400

    Create SEFIN get_data

---
+++ SEFIN_get_data.py
@@ -0,0 +1,263 @@
+import os
+import time
+import tkinter as tk
+from pathlib import Path
+from pprint import pprint
+from time import sleep
+from tkinter import messagebox
+
+import pandas as pd
+import selenium.webdriver.chrome.service as service
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+OS_NAME = 'linux' if 'posix' in os.name else 'windows'
+DESKTOP = {'linux': os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop'),
+            'windows': os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')}
+
+
+class Produto:
+    data_nf = ''
+    nr_nf = ''
+    cnpj_empresa = ''
+    nome_empresa = ''
+    descricao = ''
+    qtd = 0
+    unidade = ''
+    valor = 0
+    cod_produto = ''
+    cod_ncm = ''
+    genero = ''
+    cfop = ''
+    cod_ean = ''
+    valor_un_tributavel = 0
+    valor_un_comercializacao = 0
+    valor_aprox_tributos = 0
+    aliquota_icms = 0
+    valor_icms = 0
+
+
+
+# Main class
+class Browser:
+    def __init__(self, cupom_code) -> None:
+        self.chrome_path = ".\\GoogleChromePortable\\App\\Chrome-bin\\chrome.exe"
+        self.chromedriver_path = ".\\GoogleChromePortable\\App\\Chrome-bin\\chromedriver.exe"
+        self.cupom_code = cupom_code
+        
+        self.start_url = f"https://www.nfce.sefin.ro.gov.br/consultanfce/consulta.jsp?p={cupom_code}"
+        
+        print(self.chrome_path)
+
+        self.options = Options()
+#         self.options.add_argument('--incognito')
+        self.options.add_argument('log-level=3')  # Supress all warnings and ERRORS
+
+        self.serv = service.Service(self.chromedriver_path)
+        self.serv.start()
+        self.capabilities = {'chrome.binary': self.chrome_path}
+
+        self.driver = webdriver.Remote(self.serv.service_url, self.capabilities, options=self.options)
+        pass
+
+    def go(self, url):
+        self.driver.get(url)
+        
+  
+    def check_element(self, locator, locator_type=By.ID, timeout=10):
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located((locator_type, locator))
+            )
+            return True
+        except:
+            return False
+        
+    def wait_for_captcha(self, captcha_class="captcha-form"):
+        
+        
+        if self.check_element(captcha_class, By.CLASS_NAME):
+            print("Captcha presente. Por favor, resolva o captcha.")
+            
+            self.wait_for_element_disappear("captcha-form", By.CLASS_NAME)
+            
+            
+    def wait_for_element_disappear(self, locator, locator_type=By.ID):
+        while True:
+            try:
+                self.driver.find_element(locator_type, locator)
+                # Se o elemento for encontrado, aguarde um pouco e tente novamente
+                time.sleep(1)
+            except:
+                # Se o elemento não for encontrado (ou seja, desapareceu), saia do loop
+                break
+            
+    def wait_for_element(self, locator, locator_type=By.ID):
+        print(f'Procurando pelo elemento {locator}')
+        
+        while True:
+            if self.driver.find_element(locator_type, locator):
+                print('encontrado')
+                break
+            
+    
+    
+
+
+def CreatePopup(title, message):
+     
+     # Crie uma janela de diálogo GUI
+    root = tk.Tk()
+    # root.withdraw()  # Esconda a janela principal
+    messagebox.showinfo(title, message)
+    root.destroy()
+
+
+if __name__ == '__main__':
+            
+    cupom_code = "11230804082624003686650180001906841365894182|2|1|4|E2589715C9B0D14ECE8B5DD91E2B1DB1B203406F"
+    
+    browser = Browser(cupom_code=cupom_code)
+        
+    browser.go(browser.start_url)
+    
+    driver = browser.driver
+    
+    browser.wait_for_captcha()
+    
+    if browser.check_element("conteudo", locator_type=By.ID):
+        print("Elemento 'conteudo' encontrado. Continuando...")
+        
+        driver.find_element(By.XPATH, '//*[@id="botao_detalhada"]/a').click()
+        
+        
+        browser.wait_for_element("sefin_NFCe", By.ID)
+                
+        
+        tabela_dados_nfe = None
+        linhas_nfe = None
+        tabela_dados_nfe = driver.find_element(By.ID, 'sefin_NFCe')
+        linhas_nfe = tabela_dados_nfe.find_elements(By.TAG_NAME, 'tr')
+        
+        data_emissao = driver.find_elements(By.XPATH, '//*[@id="NFe"]/fieldset[1]/table/tbody/tr/td[4]/span')[0].text
+        total_nf = driver.find_elements(By.XPATH, '//*[@id="NFe"]/fieldset[1]/table/tbody/tr/td[6]/span')[0].text
+        cnpj_empresa = driver.find_elements(By.XPATH, '//*[@id="NFe"]/fieldset[2]/table/tbody/tr/td[1]/span')[0].text
+        nome_empresa = driver.find_elements(By.XPATH, '//*[@id="NFe"]/fieldset[2]/table/tbody/tr/td[2]/span')[0].text
+        numero_nf = driver.find_elements(By.XPATH, '//*[@id="NFe"]/fieldset[1]/table/tbody/tr/td[3]/span')[0].text
+        print(data_emissao, total_nf, cnpj_empresa, nome_empresa, numero_nf)
+        
+        
+        ### Aba Produtos
+        driver.find_element(By.XPATH, '//*[@id="tab_tipo_aba"]/li[4]/a').click()
+        
+        browser.wait_for_element("Prod", By.ID)
+        
+        div_Prod = driver.find_element(By.ID, 'Prod')
+        tabela_produtos = div_Prod.find_elements(By.TAG_NAME, 'table')
+        
+        lista_produtos = []
+
+        # Percorra cada tabela e altere o atributo 'style'
+        for tabela in tabela_produtos:
+            driver.execute_script("arguments[0].style.display = 'table';", tabela)
+            
+    
+        # for tabela in tabela_produtos:
+            class_name = tabela.get_attribute('class')
+
+            if 'toggle' in class_name or 'toggable' in class_name:
+                style = tabela.get_attribute('style')
+            
+                if 'display: none;' in style:
+                    print('Detalhes fechado, clicando...')
+                    tabela.click()
+                
+                if 'toggle' in class_name:
+            
+                    tabela_toggle = tabela.find_elements(By.TAG_NAME, 'td')
+            
+                    ### Cria novo produto        
+                    novo_produto =  Produto()
+                    ### dados do NF
+                    
+                    novo_produto.data_nf = data_emissao
+                    novo_produto.nr_nf = numero_nf
+                    novo_produto.cnpj_empresa = cnpj_empresa
+                    novo_produto.nome_empresa = nome_empresa            
+
+                    print(tabela_toggle[1].text)
+
+                    
+                    novo_produto.descricao = tabela_toggle[1].text
+                    novo_produto.qtd = tabela_toggle[2].text
+                    novo_produto.unidade = tabela_toggle[3].text
+                    novo_produto.valor = tabela_toggle[4].text
+
+                    print(f'Carregando dados do produto {novo_produto.descricao}')
+
+                
+            
+                elif 'toggable' in class_name:
+                    print('A tabela é toggable.')
+
+                    cod_ncm = None
+                    cfop = None
+                    
+                    inside_table = tabela.find_elements(By.TAG_NAME, 'table')
+
+                    table0 = inside_table[0].find_elements(By.TAG_NAME, 'td')
+                    table1 = inside_table[1].find_elements(By.TAG_NAME, 'td')
+                    
+                    novo_produto.cod_produto = table0[0].find_element(By.TAG_NAME, 'span').text
+                    novo_produto.cod_ncm = table0[1].find_element(By.TAG_NAME, 'span').text
+                    novo_produto.genero = table0[2].find_element(By.TAG_NAME, 'span').text
+                    novo_produto.cfop = table0[4].find_element(By.TAG_NAME, 'span').text
+
+                    novo_produto.cod_ean = table1[4].find_element(By.TAG_NAME, 'span').text
+
+                    novo_produto.valor_un_comercializacao = table1[7].find_element(By.TAG_NAME, 'span').text
+                    novo_produto.valor_un_tributavel = table1[8].find_element(By.TAG_NAME, 'span').text
+                    
+                    novo_produto.valor_aprox_tributos = table1[12].find_element(By.TAG_NAME, 'span').text
+
+                    novo_produto.aliquota_icms = table1[19].find_element(By.TAG_NAME, 'span').text
+                    novo_produto.valor_icms = table1[20].find_element(By.TAG_NAME, 'span').text
+                    
+                    lista_produtos.append(novo_produto)
+                    
+                    
+
+        df = pd.DataFrame([vars(p) for p in lista_produtos])
+        df['qtd'] = df['qtd'].str.replace(',', '.').astype(float)
+        df['valor'] = df['valor'].str.replace(',', '.').astype(float)
+        df['valor_un_comercializacao'] = df['valor_un_comercializacao'].str.replace(',', '.').astype(float)
+        df['valor_un_tributavel'] = df['valor_un_tributavel'].str.replace(',', '.').astype(float)
+        df['valor_aprox_tributos'] = df['valor_aprox_tributos'].str.replace(',', '.').astype(float)
+        
+        
+        df.to_excel(f'cupom-{numero_nf}.xlsx')
+                                    
+                
+        
+                
+        
+                
+        
+    else:
+        print("Elemento 'conteudo' não encontrado.")
+    
+          
+    
+
+
+    
+        
+    
+    
+        
+    
+    
+
